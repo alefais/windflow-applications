4733078b8ea563e1e903ee0850cb3796cdc558f7
Alessandra <alessandra.fais@gmail.com>
2019-05-07 14:51:04 +0200

    Markov model parameters

--- FraudDetection/util/constants.hpp
+++ FraudDetection/util/constants.hpp
@@ -1,7 +1,7 @@
 /**
  *  @file    constants.hpp
  *  @author  Alessandra Fais
- *  @date    04/05/2019
+ *  @date    07/05/2019
  *
  *  @brief Definition of useful constants
  */
@@ -9,13 +9,29 @@
 #ifndef FRAUDDETECTION_CONSTANTS_HPP
 #define FRAUDDETECTION_CONSTANTS_HPP
 
-unsigned long app_run_time = 10 * 1000000L; // 10 seconds
+#include <string>
 
+using namespace std;
+
+/// application run time (source generates the stream for app_run_time seconds, then sends out EOS)
+unsigned long app_run_time = 60 * 1000000L; // 60 seconds
+
+/// components and topology names
 const string topology_name = "FraudDetection";
-const string source_name = "source";
+const string heavy_source_name = "heavy_source";
+const string light_source_name = "light_source";
 const string predictor_name = "predictor";
 const string sink_name = "sink";
 
+/// Markov model parameters
+typedef enum { MISS_PROBABILITY, MISS_RATE, ENTROPY_REDUCTION } detection_algorithm;
 
+size_t _records_win_size = 5;
+size_t _state_position = 1;
+detection_algorithm _alg = MISS_PROBABILITY;
+double _threshold = 0.96;
+
+const string _model_file = "/home/fais/data/app/fd/model.txt";
+const string _input_file = "/home/fais/data/app/fd/credit-card.dat";
 
 #endif //FRAUDDETECTION_CONSTANTS_HPP
