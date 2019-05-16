dad15bee879beb73b0e5884414d13e9e92bc9c79
Alessandra <alessandra.fais@gmail.com>
2019-05-16 19:56:30 +0200

    Added all the nodes to the topology (use WF_Map version of the calculator node)

--- SpikeDetection/src/test_main_map/main_map.cpp
+++ SpikeDetection/src/test_main_map/main_map.cpp
@@ -1,7 +1,7 @@
 /**
- *  @file    main.cpp
+ *  @file    main_map.cpp
  *  @author  Alessandra Fais
- *  @date    14/05/2019
+ *  @date    16/05/2019
  *
  *  @brief main of the SpikeDetection application
  */
@@ -18,14 +18,14 @@
 #include "../../includes/util/constants.hpp"
 #include "../../includes/util/tuple.hpp"
 #include "../../includes/nodes/source.hpp"
+#include "../../includes/nodes/average_calculator_map.hpp"
+#include "../../includes/nodes/detector.hpp"
+#include "../../includes/nodes/sink.hpp"
 
 using namespace std;
 
-/// type of the input records: < date_value, time_value, epoch_value, device_id_value, temp_value, humid_value, light_value, voltage_value>
+// type of the input records: < date_value, time_value, epoch_value, device_id_value, temp_value, humid_value, light_value, voltage_value>
 using record_t = tuple<string, string, int, int, double, double, double, double>;
-
-/// defines information contained in each record in the dataset
-typedef enum { DATE_FIELD, TIME_FIELD, EPOCH_FIELD, DEVICE_ID_FIELD, TEMP_FIELD, HUMID_FIELD, LIGHT_FIELD, VOLT_FIELD } record_field;
 
 vector<record_t> parsed_file;               // contains data extracted from the input file
 vector<tuple_t> dataset;                    // contains all the tuples in memory
@@ -45,7 +45,7 @@ void parse_dataset(const string& file_path) {
         size_t incomplete_records = 0;  // counter of the incomplete records
 
         string line;
-        while (getline(file, line) && all_records < 50) {
+        while (getline(file, line)) {
             // process file line
             int token_count = 0;
             vector<string> tokens;
@@ -73,11 +73,10 @@ void parse_dataset(const string& file_path) {
                            atof(tokens.at(VOLT_FIELD).c_str()));
                 parsed_file.push_back(r);
 
-                // insert the key in the map (if it is not present)
+                // insert the key device_id in the map (if it is not present)
                 if (key_occ.find(get<DEVICE_ID_FIELD>(r)) == key_occ.end()) {
                     key_occ.insert(make_pair(get<DEVICE_ID_FIELD>(r), 0));
                 }
-
             } else
                 incomplete_records++;
 
@@ -85,15 +84,7 @@ void parse_dataset(const string& file_path) {
         }
         file.close();
 
-        /*cout << "[main] parsed " << all_records << " records ("
-             << incomplete_records << " incomplete, "
-             << all_records - incomplete_records << " valid)" << endl;
-        for (auto r : parsed_file)
-            cout << get<DEVICE_ID_FIELD>(r) << " - "
-                 << get<TEMP_FIELD>(r) << " - "
-                 << get<HUMID_FIELD>(r) << " - "
-                 << get<LIGHT_FIELD>(r) << " - "
-                 << get<VOLT_FIELD>(r) << endl;*/
+        //print_parsing_info(parsed_file, all_records, incomplete_records);
     }
 }
 
@@ -108,6 +99,8 @@ void create_tuples() {
         // create tuple
         auto record = parsed_file.at(next_tuple_idx);
         tuple_t t;
+
+        // select the value of the field the user chose to monitor (parameter set in constants.hpp)
         if (_field == TEMPERATURE)
             t.property_value = get<TEMP_FIELD>(record);
         else if (_field == HUMIDITY)
@@ -116,6 +109,7 @@ void create_tuples() {
             t.property_value = get<LIGHT_FIELD>(record);
         else if (_field == VOLTAGE)
             t.property_value = get<VOLT_FIELD>(record);
+
         t.incremental_average = 0;
         t.key = get<DEVICE_ID_FIELD>(record);
         t.id = (key_occ.find(get<DEVICE_ID_FIELD>(record)))->second++;
@@ -193,16 +187,13 @@ int main(int argc, char* argv[]) {
         exit(EXIT_SUCCESS);
     }
 
+    /// print application description
+    print_app_descr(file_path, source_par_deg, average_par_deg, detector_par_deg, sink_par_deg, rate);
+
     /// data pre-processing
     parse_dataset(file_path);
     create_tuples();
-    cout << "[main] dataset size: " << dataset.size() << endl;
-    /*for (auto t : dataset)
-        cout << t.property_value << " - "
-             << t.incremental_average << " - "
-             << t.key << " - "
-             << t.id << " - "
-             << t.ts << endl;*/
+    //print_dataset(dataset);
 
     /// application starting time
     unsigned long app_start_time = current_time_usecs();
@@ -214,12 +205,31 @@ int main(int argc, char* argv[]) {
             .withName(source_name)
             .build();
 
-    /// print application description
-    print_app_descr(file_path, source_par_deg, average_par_deg, detector_par_deg, sink_par_deg, rate);
+    Average_Calculator_Map_Functor avg_calc_functor;
+    Map average_calculator = Map_Builder(avg_calc_functor)
+            .withParallelism(average_par_deg)
+            .withName(avg_calc_name)
+            .enable_KeyBy()
+            .build();
+
+    Detector_Functor detector_functor;
+    Filter detector = Filter_Builder(detector_functor)
+            .withParallelism(detector_par_deg)
+            .withName(detector_name)
+            .build();
+
+    Sink_Functor sink_functor(rate, app_start_time);
+    Sink sink = Sink_Builder(sink_functor)
+            .withParallelism(sink_par_deg)
+            .withName(sink_name)
+            .build();
 
     /// create the multi pipe
     MultiPipe topology(topology_name);
     topology.add_source(source);
+    topology.add(average_calculator);
+    topology.add(detector);
+    topology.add_sink(sink);
     if (topology.run_and_wait_end() < 0)
         cerr << app_error << endl;
     else
