6d41807ff98c4dd8f52478c5fada083152a2c98e
Alessandra <alessandra.fais@gmail.com>
2019-05-16 19:57:43 +0200

    Added sink logic

---
+++ SpikeDetection/includes/nodes/sink.hpp
@@ -0,0 +1,84 @@
+/**
+ *  @file    sink.hpp
+ *  @author  Alessandra Fais
+ *  @date    16/05/2019
+ *
+ *  @brief Sink node that receives and prints the results
+ */
+
+#ifndef SPIKEDETECTION_SINK_HPP
+#define SPIKEDETECTION_SINK_HPP
+
+#include <iomanip>
+#include <ff/ff.hpp>
+#include "../util/cli_util.hpp"
+#include "../util/tuple.hpp"
+
+/**
+ *  @class Sink_Functor
+ *
+ *  @brief Defines the logic of the Sink
+ */
+class Sink_Functor {
+private:
+    int rate;                               // stream generation rate
+    unsigned long app_start_time;
+    size_t processed;                       // tuples counter
+    vector<unsigned long> tuple_latencies;  // contains the latency of each received tuple
+
+    /**
+     * Evaluate the average latency value (average time needed by a tuple in order to
+     * traverse the whole pipeline).
+     *
+     * @return average latency
+     */
+    double get_average_latency() {
+        if (rate == -1) return 0.0;
+        unsigned long acc = 0L;
+        for (long tl : tuple_latencies) {
+            acc += tl;
+        }
+        return ((double)acc / tuple_latencies.size());
+    }
+
+public:
+
+    /**
+     *  @brief Constructor
+     *
+     *  @param _rate stream generation rate
+     *  @param _app_start_time application starting time
+     */
+    Sink_Functor(const int _rate,
+                 const unsigned long _app_start_time):
+            rate(_rate),
+            app_start_time(_app_start_time),
+            processed(0) {}
+
+    /**
+     * @brief Print results and evaluate average latency (when required)
+     *
+     * @param t input tuple
+     */
+    void operator()(optional<tuple_t>& t) {
+        if (t) {
+            //print_tuple("[Sink] received tuple: ", *t);
+
+            // evaluate tuple latency
+            if (rate != -1) {
+                unsigned long tuple_latency = current_time_usecs() - (app_start_time + (*t).ts);
+                tuple_latencies.insert(tuple_latencies.end(), tuple_latency);
+            }
+            processed++;        // tuples counter
+        } else {     // EOS
+            if (processed != 0) {
+                cout << "[Sink] processed tuples: " << processed
+                     << ", average latency: " << fixed << setprecision(5)
+                     << get_average_latency()// / 1000 << " ms" << endl;
+                     << " usecs" << endl;
+            } else cout << "[Sink] processed tuples: " << processed << endl;
+        }
+    }
+};
+
+#endif //SPIKEDETECTION_SINK_HPP
