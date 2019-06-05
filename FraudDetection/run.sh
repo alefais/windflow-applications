dce713af14d26035d382ba121d400764fa135d27
Alessandra <alessandra.fais@gmail.com>
2019-06-05 11:30:52 +0200

    Updated test script

--- FraudDetection/run.sh
+++ FraudDetection/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     04/06/2019
+# @date     05/06/2019
 
 ############################################## create test directories #################################################
 
@@ -29,7 +29,7 @@ do
     do
         printf "\ntest_light_source --nsource $nsource --npred $npred --nsink $npred\n\n"
 
-        ./main_light_source --file ~/data/app/fd/credit-card.dat --nsource $nsource --npredictor $npred --nsink $npred --rate -1 | tee ../tests/output_fd60s_light_bounded/main_$nsource_$npred.log
+        ./main_light_source --file ~/data/app/fd/credit-card.dat --nsource $nsource --npredictor $npred --nsink $npred --rate -1 | tee ../tests/output_fd60s_light_bounded/main_$nsource-$npred.log
     done
 done
 
@@ -37,14 +37,14 @@ done
 
 printf "Extracting bandwidth and latency values\n"
 
-#for filename in tests/output_fd60s_light_bounded/*.log; do
+#for filename in ../tests/output_fd60s_light_bounded/*.log; do
 #    if [ ! -e "$filename" ]; then continue; fi
-#    grep "\[SUMMARY\] bandwidth" "$filename" | awk  -F'[, ]' '{ print $4 }' | tee tests/output_fd60s_light_bounded/$(basename "$filename" .log)_bw.txt
-#	grep "\[SUMMARY\] average latency" "$filename" | awk  -F'[, ]' '{ print $5 }' | tee tests/output_fd60s_light_bounded/$(basename "$filename" .log)_latency.txt
+#    grep "\[SUMMARY\] bandwidth" "$filename" | awk  -F'[, ]' '{ print $4 }' | tee ../tests/output_fd60s_light_bounded/$(basename "$filename" .log)_bw.txt
+#	grep "\[SUMMARY\] average latency" "$filename" | awk  -F'[, ]' '{ print $5 }' | tee ../tests/output_fd60s_light_bounded/$(basename "$filename" .log)_latency.txt
 #done
 
-for filename in tests/output_fd60s_light_bounded/*.log; do
+for filename in ../tests/output_fd60s_light_bounded/*.log; do
     if [ ! -e "$filename" ]; then continue; fi
-    grep "\[SUMMARY\] bandwidth" "$filename" | awk  -F'[, ]' '{ print $4 }' >> tests/output_fd60s_light_bounded/bandwidth.txt
-	grep "\[SUMMARY\] average latency" "$filename" | awk  -F'[, ]' '{ print $5 }' >> tests/output_fd60s_light_bounded/latency.txt
+    grep "\[SUMMARY\] bandwidth" "$filename" | awk  -F'[, ]' '{ print $4 }' >> ../tests/output_fd60s_light_bounded/bandwidth.txt
+	grep "\[SUMMARY\] average latency" "$filename" | awk  -F'[, ]' '{ print $5 }' >> ../tests/output_fd60s_light_bounded/latency.txt
 done
