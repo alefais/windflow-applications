1b30b04bd6a9fbede82f21256fbe12429a809ad8
Alessandra <alessandra.fais@gmail.com>
2019-06-07 21:10:57 +0200

    Modified fetch_incr method to return updated word occ value instead of old value

--- WordCount/includes/util/thread_safe_map.hpp
+++ WordCount/includes/util/thread_safe_map.hpp
@@ -3,14 +3,13 @@
  *  @author  Alessandra Fais
  *  @date    07/06/2019
  *
- *  @brief Class implementing thread safe unordered map
+ *  @brief Class implementing thread safe access to unordered map (shared state accessed by splitter replicas)
  *
  *  The class is used to count the number of occurrences of each word in the whole input file.
- *  All the replicas of the splitter node increment the counter value each time the
- *  corresponding word is encountered and processed. The data structure is used to implement a
- *  shared state: all the splitter replicas concurrently access the data structure to read and write.
- *  hat guarantees that each splitter replica put the right value in the id field
- *  of the emitted tuples of type result_t.
+ *  All the replicas of the splitter node increment the counter value each time the corresponding
+ *  word is encountered and processed. The occurrences value read from the map is used to fill the
+ *  id field of the emitted tuples of type result_t. In this way the sink will always receive correct
+ *  values of the id fields for all the keys.
  */
 #ifndef WORDCOUNT_THREAD_SAFE_MAP_HPP
 #define WORDCOUNT_THREAD_SAFE_MAP_HPP
@@ -32,15 +31,9 @@ public:
         unique_lock<mutex> lock(m);
         if (word_occ.find(word) == word_occ.end())  // insert the word in the map (if it is not present)
             word_occ.insert(make_pair(word, 0));
-        long old_value = word_occ.at(word);
         word_occ.at(word)++;
 
-        return old_value;
-    }
-
-    long get(const string& word) {
-        unique_lock<mutex> lock(m);
-        return word_occ.at(word);
+        return word_occ.at(word);                   // return updated counter value
     }
 
     ~Thread_Safe_Map() {}
