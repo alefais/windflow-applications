aea9d58efacfceb6db0dc63289376c0269ca0a96
Alessandra <alessandra.fais@gmail.com>
2019-07-08 15:13:06 +0200

    Updated SD tests: case unchained operators

--- SpikeDetection/extract_results.sh
+++ SpikeDetection/extract_results.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     18/06/2019
+# @date     08/07/2019
 
 ############################################### extract results ########################################################
 
-printf "Extracting bandwidth and latency values\n"
+printf "Extracting bandwidth and latency values for tests map, bounded buffer\n"
 
 NTHREADS=32
 
 NSOURCE_MAX=8
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
-    NDET_MAX=$((NTHREADS-nsource))
+    NDET_MAX=$((NTHREADS-nsource-2))
     for ndet in $(seq 1 $NDET_MAX);
     do
-        printf "extract from tests/output_60s_map_bounded/main_$nsource-$ndet.log\n\n"
+        printf "extract from tests/output_60s_map_bounded/main_$nsource-$ndet-1-1.log\n\n"
 
-        grep "\[SUMMARY\] bandwidth" tests/output_60s_map_bounded/main_$nsource-$ndet.log | awk  -F'[, ]' '{ print $3 }' >> tests/output_60s_map_bounded/bandwidth.txt
-	    grep "\[SUMMARY\] average latency" tests/output_60s_map_bounded/main_$nsource-$ndet.log | awk  -F'[, ]' '{ print $6 }' >> tests/output_60s_map_bounded/latency.txt
+        grep "\[SUMMARY\] bandwidth" tests/output_60s_map_bounded/main_$nsource-$ndet-1-1.log | awk  -F'[, ]' '{ print $3 }' >> tests/output_60s_map_bounded/bandwidth.txt
+	    grep "\[SUMMARY\] average latency" tests/output_60s_map_bounded/main_$nsource-$ndet-1-1.log | awk  -F'[, ]' '{ print $6 }' >> tests/output_60s_map_bounded/latency.txt
     done
 done
--- SpikeDetection/run.sh
+++ SpikeDetection/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     18/06/2019
+# @date     08/07/2019
 
 ############################################## create test directories #################################################
 
@@ -16,7 +16,7 @@ fi
 
 cd bin
 
-printf "Running tests map bounded buffer\n"
+printf "Running tests map, bounded buffer\n"
 
 NCORES=16
 NTHREADS=32
@@ -24,12 +24,12 @@ NTHREADS=32
 NSOURCE_MAX=8
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
-    NDET_MAX=$((NTHREADS-nsource))
+    NDET_MAX=$((NTHREADS-nsource-2))
     for ndet in $(seq 1 $NDET_MAX);
     do
-        printf "\ntest_map --nsource $nsource --naverage $ndet --ndetector $ndet --nsink $ndet\n\n"
+        printf "\ntest_map --nsource $nsource --naverage $ndet --ndetector 1 --nsink 1\n\n"
 
-        ./main_map --nsource $nsource --naverage $ndet --ndetector $ndet --nsink $ndet --rate -1 | tee ../tests/output_60s_map_bounded/main_$nsource-$ndet.log
+        ./main_map --nsource $nsource --naverage $ndet --ndetector 1 --nsink 1 --rate -1 | tee ../tests/output_60s_map_bounded/main_$nsource-$ndet-1-1.log
     done
 done
 
