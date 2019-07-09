3b62085ad0728733213470292976a7ceb384ca4c
Alessandra <alessandra.fais@gmail.com>
2019-07-09 13:13:32 +0200

    Updated FD tests: chained version

--- FraudDetection/extract_results_chained.sh
+++ FraudDetection/extract_results_chained.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     08/07/2019
+# @date     09/07/2019
 
 ############################################### extract results ########################################################
 
 printf "Extracting bandwidth and latency values for tests light source, bounded buffer, chained (predictor + sink)\n"
 
 NTHREADS=32
-
 NSOURCE_MAX=5
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
     NPRED_MAX=$((NTHREADS-nsource))
     for npred in $(seq 1 $NPRED_MAX);
     do
-        printf "extract from tests/output_60s_light_bounded_chained/main_$nsource-$npred-$npred.log\n\n"
+        printf "extract from tests/output_60s_bounded_chained/main_$nsource-$npred-$npred.log\n\n"
 
-        grep "\[SUMMARY\] bandwidth" tests/output_60s_light_bounded_chained/main_$nsource-$npred-$npred.log | awk  -F'[, ]' '{ print $4 }' >> tests/output_60s_light_bounded_chained/bandwidth.txt
-	    grep "\[SUMMARY\] average latency" tests/output_60s_light_bounded_chained/main_$nsource-$npred-$npred.log | awk  -F'[, ]' '{ print $7 }' >> tests/output_60s_light_bounded_chained/latency.txt
+        grep "\[SUMMARY\] bandwidth" tests/output_60s_bounded_chained/main_$nsource-$npred-$npred.log | awk  -F'[, ]' '{ print $4 }' >> tests/output_60s_bounded_chained/bandwidth.txt
+	    grep "\[SUMMARY\] average latency" tests/output_60s_bounded_chained/main_$nsource-$npred-$npred.log | awk  -F'[, ]' '{ print $7 }' >> tests/output_60s_bounded_chained/latency.txt
     done
 done
--- FraudDetection/run_chained.sh
+++ FraudDetection/run_chained.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     08/07/2019
+# @date     09/07/2019
 
 ############################################## create test directories #################################################
 
 if [ ! -d tests ]; then
     mkdir tests
 fi
-if [ ! -d tests/output_60s_light_bounded_chained ]; then
-    mkdir tests/output_60s_light_bounded_chained
+if [ ! -d tests/output_60s_bounded_chained ]; then
+    mkdir tests/output_60s_bounded_chained
 fi
 
 #################################################### run tests #########################################################
@@ -18,17 +18,17 @@ cd bin
 
 printf "Running tests light source, bounded buffer, chained (predictor + sink)\n"
 
-NCORES=16
 NTHREADS=32
-
 NSOURCE_MAX=5
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
     NPRED_MAX=$((NTHREADS-nsource))
     for npred in $(seq 1 $NPRED_MAX);
     do
-        printf "\ntest_light_source --nsource $nsource --npred $npred --nsink $npred\n\n"
+        printf "\ntest_light_source_chained --nsource $nsource --npred $npred --nsink $npred\n\n"
 
-        ./main_light_source --nsource $nsource --npredictor $npred --nsink $npred --rate -1 | tee ../tests/output_60s_light_bounded_chained/main_$nsource-$npred-$npred.log
+        ./main_light_source_chained --nsource $nsource --npredictor $npred --nsink $npred --rate -1 | tee ../tests/output_60s_bounded_chained/main_$nsource-$npred-$npred.log
     done
 done
+
+cd ..
