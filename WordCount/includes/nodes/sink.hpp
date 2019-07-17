33c5a3abdaeb3393361580cb8494f459fcc60187
Alessandra <alessandra.fais@gmail.com>
2019-07-17 16:46:58 +0200

    Added evaluation of latency statistics using alglib library functionalities

--- WordCount/includes/nodes/sink.hpp
+++ WordCount/includes/nodes/sink.hpp
@@ -1,7 +1,7 @@
 /**
  *  @file    sink.hpp
  *  @author  Alessandra Fais
- *  @date    08/07/2019
+ *  @date    17/07/2019
  *
  *  @brief Sink node that receives and prints the number of occurrences of each word in the input text
  */
@@ -9,13 +9,17 @@
 #ifndef WORDCOUNT_SINK_HPP
 #define WORDCOUNT_SINK_HPP
 
+#include <algorithm>
 #include <iomanip>
 #include <ff/ff.hpp>
+#include "statistics.h"
 #include "../../includes/util/atomic_double.hpp"
 #include "../util/cli_util.hpp"
 #include "../util/result.hpp"
 
-extern atomic<long> total_bytes;
+using namespace std;
+
+extern Atomic_Double total_MB;
 extern atomic<long> total_words;
 extern Atomic_Double average_latency_sum;
 extern atomic<int> sink_zero_processed;
@@ -29,23 +33,62 @@ class Sink_Functor {
 private:
     int rate;                               // stream generation rate
     unsigned long app_start_time;
-    size_t processed;                       // tuples counter
-    vector<unsigned long> tuple_latencies;  // contains the latency of each received tuple
-    long long bytes_sum;
+    unsigned long current_time;
+    size_t words;                           // tuples (words) counter
+    double bytes_sum;                       // bytes counter
+    vector<double> tuple_latencies;         // contains the latency (ms) of each received tuple
 
     /**
-     * Evaluate the average latency value (average time needed by a tuple in order to
-     * traverse the whole pipeline).
-     *
-     * @return average latency
+     * Evaluate latency statistics:
+     * - mean value (average time needed by a tuple in order to traverse the whole pipeline),
+     * - 5th, 25th, 50th, 75th, 95th percentiles,
+     * - minimum value,
+     * - maximum value.
      */
-    double get_average_latency() {
-        //if (rate == -1) return 0.0; // always evaluate latency with FF_BOUNDED_BUFFER set
-        unsigned long acc = 0L;
-        for (long tl : tuple_latencies) {
-            acc += tl;
-        }
-        return ((double)acc / tuple_latencies.size());
+    double compute_latency_statistics() {
+        // vector containing all latency values
+        alglib::real_1d_array tuple_lat_array;
+        tuple_lat_array.setcontent(tuple_latencies.size(), &(tuple_latencies.at(0)));
+
+        // percentiles
+        double perc_5 = 0.05;
+        double perc_25 = 0.25;
+        double perc_50 = 0.5;
+        double perc_75 = 0.75;
+        double perc_95 = 0.95;
+
+        // statistics
+        double mean;
+        double min;
+        double perc_5_val;
+        double perc_25_val;
+        double perc_50_val;
+        double perc_75_val;
+        double perc_95_val;
+        double max;
+
+        // statistics computation
+        mean = alglib::samplemean(tuple_lat_array);
+        alglib::samplepercentile(tuple_lat_array, perc_5, perc_5_val);
+        alglib::samplepercentile(tuple_lat_array, perc_25, perc_25_val);
+        alglib::samplepercentile(tuple_lat_array, perc_50, perc_50_val);
+        alglib::samplepercentile(tuple_lat_array, perc_75, perc_75_val);
+        alglib::samplepercentile(tuple_lat_array, perc_95, perc_95_val);
+        min = *min_element(tuple_latencies.begin(), tuple_latencies.end());
+        max = *max_element(tuple_latencies.begin(), tuple_latencies.end());
+
+        // latency summary
+        cout << "[Sink] latency (ms): "
+             << mean << " (mean) "
+             << min << " (min) "
+             << perc_5_val << " (5th) "
+             << perc_25_val << " (25th) "
+             << perc_50_val << " (50th) "
+             << perc_75_val << " (75th) "
+             << perc_95_val << " (95th) "
+             << max << " (max)." << endl;
+
+        return mean;
     }
 
 public:
@@ -60,41 +103,45 @@ public:
                  const unsigned long _app_start_time):
             rate(_rate),
             app_start_time(_app_start_time),
-            processed(0) {}
+            current_time(_app_start_time),
+            words(0),
+            bytes_sum(0) {}
 
     /**
-     * @brief Print results and evaluate average latency (when required)
+     * @brief Print results and evaluate latency statistics
      *
-     * @param t input tuple
+     * @param r input tuple
      */
     void operator()(optional<result_t>& r) {
         if (r) {
             //print_result("[Sink] received tuple: ", *r);
 
-            bytes_sum += (*r).bytes;
+            words++;                            // tuples counter
+            bytes_sum += (*r).key.length();     // bytes counter
 
-            // evaluate tuple latency (always evaluate latency with FF_BOUNDED_BUFFER set)
-            //if (rate != -1) {
+            // always evaluate latency when compiling with FF_BOUNDED_BUFFER MACRO set
+            unsigned long tuple_latency = current_time_usecs() - (app_start_time + (*r).ts);    // latency (usecs)
+            tuple_latencies.insert(tuple_latencies.end(), (double)tuple_latency / 1000L);       // latency (ms)
 
-            unsigned long tuple_latency = current_time_usecs() - (app_start_time + (*r).ts);
-            tuple_latencies.insert(tuple_latencies.end(), tuple_latency);
-
-            //}
-            processed++;        // tuples counter
+            current_time = current_time_usecs();
         } else {     // EOS
-            if (processed != 0) {
-                /*cout << "[Sink] processed tuples: " << processed
-                     << " words (" << bytes_sum << " bytes)"
-                     << ", average latency: " << fixed << setprecision(5)
-                     << get_average_latency()// / 1000 << " ms" << endl;
-                     << " usecs" << endl;*/
+            if (words != 0) {
+                unsigned long t_elapsed = (current_time - app_start_time) / 1000000L;
 
-                total_bytes.fetch_add(bytes_sum);
-                total_words.fetch_add(processed);
-                average_latency_sum.fetch_add(get_average_latency()); // add average latency value (useconds)
+                cout << "[Sink] processed: "
+                     << words << " (words) "
+                     << (bytes_sum / 1048576) << " (MB), "
+                     << "bandwidth: "
+                     << words / t_elapsed << " (words/s) "
+                     << (bytes_sum / 1048576) / t_elapsed << " (MB/s)." << endl;
+
+                double average_latency = compute_latency_statistics();
+
+                total_MB.fetch_add(bytes_sum / 1048576);
+                total_words.fetch_add(words);
+                average_latency_sum.fetch_add(average_latency);
             } else {
-                //cout << "[Sink] processed tuples: " << processed << endl;
-
+                //cout << "[Sink] processed: " << words << endl;
                 sink_zero_processed.fetch_add(1);
             }
         }
