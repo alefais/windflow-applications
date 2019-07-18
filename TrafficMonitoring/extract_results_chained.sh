6626b9a0ec7893538d189d8ea1e87e368481d600
Alessandra <alessandra.fais@gmail.com>
2019-07-18 17:25:37 +0200

    Updated test scripts for TM application: chained and unchained version

--- TrafficMonitoring/extract_results_chained.sh
+++ TrafficMonitoring/extract_results_chained.sh
@@ -1,23 +1,60 @@
 #!/usr/bin/env bash
 
 # @author   Alessandra Fais
-# @date     08/07/2019
+# @date     18/07/2019
 
 ############################################### extract results ########################################################
 
-printf "Extracting bandwidth and latency values for tests map bounded buffer, chained (calculator + sink)\n"
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+NORMAL=$(tput sgr0)
+
+printf "${GREEN}Extracting bandwidth and latency values for TrafficMonitoring application\n${NORMAL}"
 
 NTHREADS=32
-
 NSOURCE_MAX=1
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
     NMATCH_MAX=$((NTHREADS-nsource-1))
-    for nmatch in $(seq 1 $NMATCH_MAX);
+    for nmatch in {0..32..2};
     do
-        printf "extract from tests/output_60s_map_bounded_chained/main_$nsource-$nmatch-1-1-10000.log\n\n"
+        if [ $nmatch -eq 0 ];
+        then
+            printf "${BLUE}extract from tests/output_60s_bounded_chained/main_$nsource-$nsource-1-1_-1.log\n\n${NORMAL}"
 
-        grep "\[SUMMARY\] bandwidth" tests/output_60s_map_bounded_chained/main_$nsource-$nmatch-1-1-10000.log | awk  -F'[, ]' '{ print $3 }' >> tests/output_60s_map_bounded_chained/bandwidth.txt
-	    grep "\[SUMMARY\] average latency" tests/output_60s_map_bounded_chained/main_$nsource-$nmatch-1-1-10000.log | awk  -F'[, ]' '{ print $6 }' >> tests/output_60s_map_bounded_chained/latency.txt
+            # bandwidth
+            grep "\[SUMMARY\] bandwidth" tests/output_60s_bounded_chained/main_$nsource-$nsource-1-1-10000.log | awk  -F'[, ]' '{ print $3 }' >> tests/output_60s_bounded_chained/bandwidth.txt
+
+	        # latency
+	        grep "\[Sink\] latency" tests/output_60s_bounded_chained/main_$nsource-$nsource-1-1_-1.log | awk  -F'[, ]' '{ print $4 " " $6 " " $8 " " $10 " " $12 " " $14 " " $16 " " $18 }' >> tests/output_60s_bounded_chained/latency.txt
+	        grep "\[Sink\] latency" tests/output_60s_bounded_chained/main_$nsource-$nsource-1-1_-1.log | awk  -F'[, ]' '{ print $4 }' >> tests/output_60s_bounded_chained/latency_mean.txt
+
+        elif [[ ($nmatch -le $NMATCH_MAX) && ($nmatch -ge $nsource) ]];
+        then
+            printf "${BLUE}extract from tests/output_60s_bounded_chained/main_$nsource-$nmatch-1-1_-1.log\n\n${NORMAL}"
+
+            # bandwidth
+            grep "\[SUMMARY\] bandwidth" tests/output_60s_bounded_chained/main_$nsource-$nmatch-1-1-10000.log | awk  -F'[, ]' '{ print $3 }' >> tests/output_60s_bounded_chained/bandwidth.txt
+
+	        # latency
+	        grep "\[Sink\] latency" tests/output_60s_bounded_chained/main_$nsource-$nmatch-1-1_-1.log | awk  -F'[, ]' '{ print $4 " " $6 " " $8 " " $10 " " $12 " " $14 " " $16 " " $18 }' >> tests/output_60s_bounded_chained/latency.txt
+	        grep "\[Sink\] latency" tests/output_60s_bounded_chained/main_$nsource-$nmatch-1-1_-1.log | awk  -F'[, ]' '{ print $4 }' >> tests/output_60s_bounded_chained/latency_mean.txt
+
+        fi
     done
 done
+
+if [ ! -d tests/output_60s_bounded_chained/logs ]; then
+    mkdir tests/output_60s_bounded_chained/logs
+fi
+
+mv tests/output_60s_bounded_chained/*.log tests/output_60s_bounded_chained/logs/
+
+if [ ! -d tests/output_60s_bounded_chained/statistics ]; then
+    mkdir tests/output_60s_bounded_chained/statistics
+fi
+
+mv tests/output_60s_bounded_chained/*.txt tests/output_60s_bounded_chained/statistics/
--- TrafficMonitoring/run_unchained.sh
+++ TrafficMonitoring/run_unchained.sh
@@ -8,8 +8,8 @@
 if [ ! -d tests ]; then
     mkdir tests
 fi
-if [ ! -d tests/output_60s_bounded_chained ]; then
-    mkdir tests/output_60s_bounded_chained
+if [ ! -d tests/output_60s_bounded_unchained ]; then
+    mkdir tests/output_60s_bounded_unchained
 fi
 
 #################################################### run tests #########################################################
@@ -23,26 +23,26 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 NORMAL=$(tput sgr0)
 
-printf "${GREEN}Running WindFlow tests for TrafficMonitoring application with bounded buffer and chaining.\n${NORMAL}"
+printf "${GREEN}Running WindFlow tests for TrafficMonitoring application with bounded buffer and no chaining.\n${NORMAL}"
 
 NTHREADS=32
 NSOURCE_MAX=1
 for nsource in $(seq 1 $NSOURCE_MAX);
 do
-    NMATCH_MAX=$((NTHREADS-nsource-1))
+    NMATCH_MAX=$((NTHREADS-nsource-2))
     for nmatch in {0..32..2};
     do
         if [ $nmatch -eq 0 ];
         then
             printf "\nwindflow_trafficmonitoring --nsource $nsource --nmatcher $nsource --ncalculator 1 --nsink 1 --rate 10000\n\n"
 
-            ./main_chained --nsource $nsource --nmatcher $nsource --ncalculator 1 --nsink 1 --rate 10000 | tee ../tests/output_60s_bounded_chained/main_$nsource-$nsource-1-1-10000.log
+            ./main_unchained --nsource $nsource --nmatcher $nsource --ncalculator 1 --nsink 1 --rate 10000 | tee ../tests/output_60s_bounded_unchained/main_$nsource-$nsource-1-1-10000.log
 
         elif [[ ($nmatch -le $NMATCH_MAX) && ($nmatch -ge $nsource) ]];
         then
             printf "\nwindflow_trafficmonitoring --nsource $nsource --nmatcher $nmatch --ncalculator 1 --nsink 1 --rate 10000\n\n"
 
-            ./main_chained --nsource $nsource --nmatcher $nmatch --ncalculator 1 --nsink 1 --rate 10000 | tee ../tests/output_60s_bounded_chained/main_$nsource-$nmatch-1-1-10000.log
+            ./main_unchained --nsource $nsource --nmatcher $nmatch --ncalculator 1 --nsink 1 --rate 10000 | tee ../tests/output_60s_bounded_unchained/main_$nsource-$nmatch-1-1-10000.log
         fi
     done
 done
